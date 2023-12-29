b53603aba70796079cba4e15916b225a1bf37ea7
muha <1student.muhammed1@gmail.com>
2023-12-29 23:02:16 +0300

    feat: add loading animation component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const LoadingAnimation = () => {
+  return (
+    <div className="flex justify-center items-center h-full row-span-5">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-red-400 "></div>
+    </div>
+  );
+};
+
+export default LoadingAnimation;
