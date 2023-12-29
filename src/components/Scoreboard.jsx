001806822dcadd06a343eb22805eef4d2ff38765
muha <1student.muhammed1@gmail.com>
2023-12-29 23:00:48 +0300

    feat: replace static text with a dynamic prop in scoreboard component

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,7 +3,7 @@ export default function ScoreBoard({score, bestScore}) {
         <div className=" w-60 h-28">
             <div className=" bg-gray-100 h-1/2 rounded-xl flex justify-center items-center opacity-65">
                 <h2 className="text-xl text-stone-900 text-center font-custom2">
-                    Score: 0 | Best Score: 0
+                    Score: {score} | Best Score: {bestScore}
                 </h2>
             </div>
         </div>
