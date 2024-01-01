0c4abbbe78fb919b736831a77da406e7e4d84528
muha <1student.muhammed1@gmail.com>
2024-01-01 14:16:23 +0300

    feat: change the loading animation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,16 @@
+import { ring } from 'ldrs'
 const LoadingAnimation = () => {
+ring.register()
   return (
     <div className="flex justify-center items-center h-full row-span-5">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-red-400 "></div>
+       <l-ring
+         size="150"
+         stroke="2"
+         bg-opacity="0"
+         speed="2" 
+         color="red" 
+           ></l-ring>
+
     </div>
   );
 };
